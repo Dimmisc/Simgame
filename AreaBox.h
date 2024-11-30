606d198cc759d320bb58719e257e5cd0958b9f39
Dimmisc <dimamyscen@gmail.com>
2024-11-30 16:56:37 +0300

    Update AreaBox.h

--- AreaBox.h
+++ AreaBox.h
@@ -2,16 +2,40 @@
 #define _AreaBox_H_
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    int posx;
+    int posy;
+    int type;
+    char name[15];
+} sprite;
+
 typedef struct{
   int mapwidth;
   int mapheight;
   int maplayots;
+  sprite *sprites[1];
+  int *effects[1];
 } Areamap;
+
+Areamap folder={0,0,0};
+
  Areamap *Area(unsigned int inwidth, unsigned int inheight, unsigned int inlayots){
-    Areamap c = {0, 1, 0,};
-    return &c;
+    Areamap folder = {inwidth, inheight, inlayots,};
+
+    return &folder;
 }
+
+Areamap *CreateSprite(char name[15]){
+    sprite c = {0,0,0};
+    strcpy(c.name, name);
+    folder.sprites[1] = &c;
+    return &folder;
+}
+
 extern Areamap;
+extern Areamap *CreateSprite();
 extern Areamap *Area();
 
 #endif
