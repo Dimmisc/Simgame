2f3462a3b681b2825ad10528ce5cc01acee410de
Dimmisc <dimamyscen@gmail.com>
2024-12-03 20:16:48 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,13 +2,18 @@
 #include "AreaBox.h"
 
 unsigned int height=100000, width=100000, layots=8;
+
 Areamap *map;
+sprite *Spritesonmap;
 
 int main(int arc, char *argv[]){
     char name[15];
+    
     map = Area(height, width, layots);
     map = CreateSprite(name);
+    Spritesonmap = ReturnSprites();
+    
     printf("Hello, World!");
-
+    
     return 0;
 }
