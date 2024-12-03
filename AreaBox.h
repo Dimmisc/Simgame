dd8a261a202f3de637170a4b0ba3d070a1d5e62e
Dimmisc <dimamyscen@gmail.com>
2024-12-03 20:18:40 +0300

    Update AreaBox.h

--- AreaBox.h
+++ AreaBox.h
@@ -7,7 +7,7 @@
 typedef struct{
     int posx;
     int posy;
-    int type;
+    int layot;
     char name[15];
 } sprite;
 
@@ -15,26 +15,34 @@ typedef struct{
   int mapwidth;
   int mapheight;
   int maplayots;
-  sprite *sprites[1];
+  sprite sprites[1];
   int *effects[1];
 } Areamap;
 
 Areamap folder={0,0,0};
+int i=0;
 
  Areamap *Area(unsigned int inwidth, unsigned int inheight, unsigned int inlayots){
-    Areamap folder = {inwidth, inheight, inlayots,};
-
+    folder.mapheight = inheight;
+    folder.mapwidth = inwidth;
+    folder.maplayots = inlayots;
     return &folder;
 }
 
 Areamap *CreateSprite(char name[15]){
-    sprite c = {0,0,0};
-    strcpy(c.name, name);
-    folder.sprites[1] = &c;
+    int n = sizeof(folder.sprites)/sizeof(folder.sprites[0]) - 1;
+    folder.sprites[n].posx = 0;
+    folder.sprites[n].posy = 0;
+    folder.sprites[n].layot = 0;
+    // добавление пустого элемента в folder.sprites (пока нету:() //
     return &folder;
 }
 
-extern Areamap;
+sprite *ReturnSprites(){
+    return folder.sprites;
+    }
+
+extern sprite *ReturnSprites();
 extern Areamap *CreateSprite();
 extern Areamap *Area();
 
