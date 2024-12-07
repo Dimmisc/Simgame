32c9c25316729af6787f4b30a75b4027953e7f88
Dimmisc <dimamyscen@gmail.com>
2024-12-07 12:08:54 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,15 +4,21 @@
 unsigned int height=100000, width=100000, layots=8;
 
 Areamap *map;
-sprite *Spritesonmap;
+sprite *Smap;
 
-int main(){
+int main(int arc, char *argv[]){
     char name[15];
-
+    strncpy(name, "Ork_deligation", 15);
+    int n=1;
     map = Area(height, width, layots);
     map = CreateSprite(name, 1, 0);
-    sprite *smap = ReturnSprites();
-
+    Smap = ReturnNSprite(n);
+    if (Smap != 0){
+        printf("%s\n", Smap->name);
+    }
+    else {
+        printf("Error in function ReturnNSprite");
+    }
     printf("Hello, World!");
     CloseArea();
     return 0;
