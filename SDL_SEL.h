b48f0aa8a4383e1c34673da7084518ed520573d1
Dimmisc <dimamyscen@gmail.com>
2024-12-16 22:48:50 +0300

    Update SDL_SEL.h

--- SDL_SEL.h
+++ SDL_SEL.h
@@ -5,28 +5,70 @@
 #include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "AreaBox.h"
 
-typedef struct SEL_ConsoleWindow{
-    SDL_Window *Window;
-    SDL_Surface *Surface;
-    SDL_Renderer *Render;
-    SDL_Texture *Texture;
+
+typedef struct SGL_ConsoleWindow{
+    SDL_Window *window;
+    SDL_Surface *surface;
+    SDL_Renderer *render;
+    sprite **shownSprites;
     char Error[30];
-    int SIZETEXTURE;
-    int LENTEXTURES;
-    int SIZESURFACE;
-    int LENSURFACES;
-}SEL_ConsoleWindow;
+    int lsS;
+} SEL_ConsoleWindow;
 
-SEL_ConsoleWindow *SEL_initMainW(ConsoleWindow *ARG){
-    int width = 100, height = 100, x = 0, y = 0;
-    ARG->Window = SDL_CreateWindow("Simagame", x, y, width, height, SDL_WINDOW_SHOWN);
-    return ARG;
+typedef struct WindowSettings{
+    char name[];
+    int width;
+    int height;
+    int x;
+    int y;
+} WindowSettings;
+
+int SEL_init(SEL_ConsoleWindow *ARG, WindowSettings *settings){
+    int succes = 0;
+    ARG->window = SDL_CreateWindow(settings.name,
+                                   settings.x,
+                                   settings.y,
+                                   settings.width,
+                                   settings.height,
+                                   SDL_WINDOW_SHOWN
+                                   );
+    if (ARG->window == NULL){
+        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else {
+        ARG->render = SDL_CreateRenderer(ARG->window, -1, SDL_RENDERER_ACCELERATED);
+        if (ARG->render == NULL){
+            printf( "Render could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else {
+            succes = 1;
+        }
+    }
+    return succes;
 }
 
-int SEL_CloseCW(ConsoleWindow *ARG){
+int SEL_UpdateScreen(SEL_ConsoleWindow *ARG){
+    int succes = 1;
+    SDL_RenderClear(ARG->render);
+    for (int i=0;i<ARG->lsS; i++)
+        SDL_RenderCopyEx(ARG->render,
+                         ARG->shownSprites[i]->spriteTexture,
+                         NULL,
+                         &ARG->shownSprites[i]->arguments->parametres,
+                         &ARG->shownSprites[i]->arguments->center,
+                         );
+    SDL_RenderPresent(ARG->render);
+    return succes;
+}
 
+int SEL_QUIT(SEL_ConsoleWindow *ARG){
+    CloseArea();
+    SDL_DestroyRenderer(ARG->render);
+    SDL_FreeSurface(ARG->surface);
     SDL_DestroyWindow( ARG->Window );
+    SDL_QUIT();
     return 0;
 }
 
