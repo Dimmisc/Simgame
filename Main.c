33fe211986b99c970b9a9d95a53210146258bf70
Dimmisc <dimamyscen@gmail.com>
2025-01-04 16:53:01 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,32 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "SDL_SEL.h"
 #include "AreaBox.h"
 
-unsigned int height=100000, width=100000, layots=8;
 
-Areamap *map;
-sprite *Smap;
-
-int main(){
-    char name[15];
-    strncpy(name, "Practicalname1", 15);
-    int n=2;
-    map = Area(height, width, layots);
-    map = CreateSprite(name, 1, 0);
-    strncpy(name, "Practicalname2", 15);
-    map = CreateSprite(name, 1, 1);
-    Smap = ReturnNSprite(n);
-    for (int i=1;i<=n;i++){
-        Smap = ReturnNSprite(i);
-        if (Smap != 0){
-            printf("%s\n", Smap->name);
-        }
-        else {
-            printf("%s\n", map->error);
-            break;
-        }
-    }
-    printf("Hello, World!\n");
-    CloseArea();
+int main(int argc, char* argv[])
+{
     return 0;
 }
